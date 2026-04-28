cafe0c3aaad495df274c95625e590ab4cb200c55
terriwm <william@terrington.id.au>
2026-04-28 15:48:00 +0930

    The rest of the code

---
+++ apriltag_from_image.py
@@ -0,0 +1,33 @@
+import cv2
+from pupil_apriltags import Detector
+
+img = cv2.imread("apriltag-real.png")
+grey_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+at_detector = Detector(families="tagCircle21h7")
+
+tags = at_detector.detect(grey_image)
+
+print(f"FOUND {len(tags)} TAGS!")
+
+for detection in tags:
+    print(detection)
+    p1 = tuple(map(int, detection.corners[3]))
+    p2 = tuple(map(int, detection.corners[2]))
+    p3 = tuple(map(int, detection.corners[1]))
+    p4 = tuple(map(int, detection.corners[0]))
+    cv2.rectangle(img, p1, p2, (0,255,0), 3)
+    cv2.line(img, p1, p2, (0,255,0), 3)
+    cv2.line(img, p2, p3, (0,255,0), 3)
+    cv2.line(img, p3, p4, (0,255,0), 3)
+    cv2.line(img, p4, p1, (0,255,0), 3)
+    print("----------------------------------------------")
+
+cv2.imshow("Highlighted Apriltag", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# print(at_detector.detect(grey_image))
+
---
+++ apriltag_from_webcam.py
@@ -0,0 +1,50 @@
+import cv2
+from pupil_apriltags import Detector
+
+CONFIDENCE_THRESHOLD = 10
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+# Setup a detector - we use the circle21h7 family of tags, other than that default settings
+at_detector = Detector(families="tagCircle21h7")
+
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    # Take the frame and make it greyscale - no need to use all the colours
+    grey_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Pass the frame into the detector
+    tags = at_detector.detect(grey_image)
+
+    # Remove all tags that are below a certain confidence value
+    tags = [tag for tag in tags if tag.decision_margin > CONFIDENCE_THRESHOLD]
+
+    for detection in tags:
+        print(detection)
+        # Convert the pixel values it an integer
+        p1 = tuple(map(int, detection.corners[3]))
+        p2 = tuple(map(int, detection.corners[2]))
+        p3 = tuple(map(int, detection.corners[1]))
+        p4 = tuple(map(int, detection.corners[0]))
+
+        # Draw lines around the apriltag - note we are using the frame and not the greyscale image here since we want to see locations on the colour preview
+        cv2.line(frame, p1, p2, (0,255,0), 3)
+        cv2.line(frame, p2, p3, (0,255,0), 3)
+        cv2.line(frame, p3, p4, (0,255,0), 3)
+        cv2.line(frame, p4, p1, (0,255,0), 3)
+
+    # Show the colour preview
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+vc.release()
