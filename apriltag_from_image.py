da3cfc7b00bc7ad0d85154c86770877743a5390f
terriwm <william@terrington.id.au>
2026-04-28 18:50:44 +0930

    Removed the rectangle draw that was causing strange thicker lines

--- apriltag_from_image.py
+++ apriltag_from_image.py
@@ -17,7 +17,6 @@ for detection in tags:
     p2 = tuple(map(int, detection.corners[2]))
     p3 = tuple(map(int, detection.corners[1]))
     p4 = tuple(map(int, detection.corners[0]))
-    cv2.rectangle(img, p1, p2, (0,255,0), 3)
     cv2.line(img, p1, p2, (0,255,0), 3)
     cv2.line(img, p2, p3, (0,255,0), 3)
     cv2.line(img, p3, p4, (0,255,0), 3)
