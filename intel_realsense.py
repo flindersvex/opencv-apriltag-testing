21dabc31b1756de2902f47806909bd18b994a034
terriwm <william@terrington.id.au>
2026-04-29 12:31:04 +0930

    Wee bit of refactoring

--- intel_realsense.py
+++ intel_realsense.py
@@ -13,6 +13,22 @@ pipeline.start(config)
 
 at_detector = Detector(families="tagCircle21h7")
 
+def draw_identifiers(frame, corners):
+    p1 = tuple(map(int, corners[3]))
+    p2 = tuple(map(int, corners[2]))
+    p3 = tuple(map(int, corners[1]))
+    p4 = tuple(map(int, corners[0]))
+    
+    # Calculate the center of the tag using numpy
+    centroid = np.mean(corners, axis=0).astype(int)
+
+    cv2.line(frame, p1, p2, (0,255,0), 3)
+    cv2.line(frame, p2, p3, (0,255,0), 3)
+    cv2.line(frame, p3, p4, (0,255,0), 3)
+    cv2.line(frame, p4, p1, (0,255,0), 3)
+    cv2.putText(frame, str(detection.tag_id), p2, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.circle(frame, centroid, 2, (0, 0, 255), 3)
+
 try:
     while True:
         frames = pipeline.wait_for_frames()
@@ -58,21 +74,8 @@ try:
             print(f"The tag with id {detection.tag_id} is {dist:.3f} meters away")
 
 
-            # Draw lines around the apriltag - note we are using the colour and not the greyscale image here since we want to see locations on the colour preview
-            cv2.line(color_image, p1, p2, (0,255,0), 3)
-            cv2.line(color_image, p2, p3, (0,255,0), 3)
-            cv2.line(color_image, p3, p4, (0,255,0), 3)
-            cv2.line(color_image, p4, p1, (0,255,0), 3)
-            cv2.putText(color_image, str(detection.tag_id), p2, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-
-            # Draw lines around the apriltag - note we are using the depth and not the greyscale image here since we want to see locations on the depth preview
-            cv2.line(depth_colormap, p1, p2, (0,255,0), 3)
-            cv2.line(depth_colormap, p2, p3, (0,255,0), 3)
-            cv2.line(depth_colormap, p3, p4, (0,255,0), 3)
-            cv2.line(depth_colormap, p4, p1, (0,255,0), 3)
-            cv2.putText(depth_colormap, str(detection.tag_id), p2, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.circle(color_image, centroid, 2, (0, 0, 255), 3)
-            cv2.circle(depth_colormap, centroid, 2, (0, 0, 255), 3)
+            draw_identifiers(color_image, detection.corners)
+            draw_identifiers(depth_colormap, detection.corners)
             
         cv2.imshow('Camera Feed', color_image)
         cv2.imshow('Depth Feed', depth_colormap)
