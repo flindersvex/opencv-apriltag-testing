b254a45ba0f8c61caba843fa24a3c9c7ef82c6dd
terriwm <william@terrington.id.au>
2026-05-06 11:10:01 +0930

    contour testing

---
+++ pin_contour.py
@@ -0,0 +1,17 @@
+import cv2
+from pupil_apriltags import Detector
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread("pin.png")
+
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+cv2.imshow("Pin - Greyscale", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
